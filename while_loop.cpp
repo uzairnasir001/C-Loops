6812525b8cc313e1c6a6733ef5c360c607b2c94b
uzairnasir001 <uzairnasir477@gmail.com>
2025-10-07 11:26:59 +0500

    While loop Some Programs

---
+++ while_loop.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//   Program 6.1
+    //  int n=1;
+    //  while (n<=5)
+    //  {
+    //     cout<<"Pakistan"<<endl;
+    //     n++;
+    //  }
+     
+    // Program 6.2
+    // int n=1;
+    // while (n<=10)
+    // {
+    //     cout<<n<<endl;
+    //     n++;
+    // }
+    
+    // Program 6.3
+    // int n=1, sum=0;
+    // while (n<=5)
+    // {
+    //     cout<<n<<endl;
+    //     sum = sum + n;
+    //     n++;
+    // }
+    // cout<<"The sum is "<<sum<<endl;
+
+    // Program 6.4
+    // int n=1;
+    // while (n<=5)
+    // {
+    //     cout<<"The number "<<n<<" square is "<<n*n<<endl;
+    //     n++;
+    // }
+    
+    // Program 6.5
+    // int n=1,num;
+    // cout<<"Enter a number:"<<endl;
+    // cin>>num;
+    // while (n<=10)
+    // {
+    //     cout<<num<<" * "<<n<<" = "<<num*n<<endl;
+    //     n++;
+    // }
+    
+    // Program 6.6
+    // int num,sum=0,digit;
+    // cout<<"Enter an integer:"<<endl;
+    // cin>>num;
+    // while (num!=0)
+    // {
+    //     digit= num%10;
+    //     sum = sum+digit;
+    //     num=num/10;
+    // }
+    // cout<<"The sum of digits is "<<sum<<endl;
+
+    // Program 6.7
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
